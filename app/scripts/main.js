d40f078f13bfd902b0640d41bb15bf0d9f02cd24
Jure Martinec <jure.martinec@gmail.com>
2015-06-10 17:01:56 +0100

    tweak file saving

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,30 +1,6 @@
 /*! mousestop - v3.0.1 - 2013-07-15 */!function(a){function b(){var b=this,c=a(this).data("mousestop");this.movement=!0,c.timeToStop&&(this.timeToStopTimer=window.setTimeout(function(){b.movement=!1,window.clearTimeout(b.timer)},c.timeToStop))}function c(){window.clearTimeout(this.timer),window.clearTimeout(this.timeToStopTimer)}function d(){var b=a(this),c=b.data("mousestop");this.movement&&(window.clearTimeout(this.timer),this.timer=window.setTimeout(function(){b.trigger("mousestop")},c.delay))}function e(b){return a.isNumeric(b)?b={delay:b}:"object"!=typeof b&&(b={}),a.extend({},a.fn.mousestop.defaults,b)}a.event.special.mousestop={setup:function(f){a(this).data("mousestop",e(f)).bind("mouseenter.mousestop",b).bind("mouseleave.mousestop",c).bind("mousemove.mousestop",d)},teardown:function(){a(this).removeData("mousestop").unbind(".mousestop")}},a.fn.mousestop=function(a,b){return"function"==typeof a&&(b=a),arguments.length>0?this.bind("mousestop",a,b):this.trigger("mousestop")},a.fn.mousestop.defaults={delay:300,timeToStop:null}}(jQuery);
 
-function b64toBlob(b64Data, contentType, sliceSize) {
-    contentType = contentType || '';
-    sliceSize = sliceSize || 512;
-
-    var byteCharacters = atob(b64Data);
-    var byteArrays = [];
-
-    for (var offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-        var slice = byteCharacters.slice(offset, offset + sliceSize);
-
-        var byteNumbers = new Array(slice.length);
-        for (var i = 0; i < slice.length; i++) {
-            byteNumbers[i] = slice.charCodeAt(i);
-        }
-
-        var byteArray = new Uint8Array(byteNumbers);
-
-        byteArrays.push(byteArray);
-    }
-
-    var blob = new Blob(byteArrays, {type: contentType});
-    return blob;
-}
-
-function uploadMedia(filename, mdata) {
+function uploadMedia(filename, blob) {
 
   $.ajax({
     type:'POST',
@@ -32,10 +8,10 @@ function uploadMedia(filename, mdata) {
     headers: { 'Authorization': 'Bearer XVeOb0MKbDwAAAAAAAAKNlnrMfkSqFFq_bK8hOfOQOI8aM58i0HI4QfcjRwK8crJ', 'Content-Type': 'image/png'},
     dataType: 'html',
     processData: false,
-    data: mdata,
-    // success: function(data) {
-    //     console.log(data);
-    // },
+    data: blob,
+    success: function(data) {
+        console.log(data);
+    },
     error: function(jqXHR, textStatus, errorThrown ) {
         console.log("Error when uploading file: " + textStatus + " because:" + errorThrown);
     }
@@ -281,11 +257,14 @@ function generateLink(coordX, coordY) {
 	// draw from svg
 	canvg("placeholder", $("<svg />").append( $("svg").clone() ).html() );
 
-	// create link
-	var imgUrl = canvas[0].toDataURL();
+  // generate image data uri
+  var blob;
+  canvas[0].toBlob(function(b) {
+    blob = b;
+  });
 
 	// create and position button
-	$("body").append('<div id="saved"><a href="'+imgUrl+'" download="treep.png">&#8677;</a></div>');
+	$("body").append('<div id="saved"><span>&#8677;</span></div>');
 
 	$("div#saved").css({
 		left: coordX-($("div#saved").width()/2),
@@ -297,106 +276,9 @@ function generateLink(coordX, coordY) {
 
 	// revert to window size
 	$('svg').attr('width', width).attr('height', height);
+
+  return blob;
 }
-
-
-/*
-var conductor = new BandJS();
-var player;
-var lockMusic = true;
-
-function changeMusic() {
-
-
-	var colorValue = [0,0];
-
-	$("svg").children().each(function() {
-
-		var fill = $(this).attr("fill");
-
-		if( fill !== undefined ) {
-			var colorRGB = hexToRgb( fill );
-
-			if( (colorRGB.r === colorRGB.g) && (colorRGB.g === colorRGB.b) && (colorRGB.r === colorRGB.b) ) {
-				colorValue[0] += getRandom(0, 100);
-				colorValue[1]++;
-			}
-			else {
-				colorValue[0] += (colorRGB.r + colorRGB.g + colorRGB.b);
-				colorValue[1]++;
-			}
-		}
-	});
-
-	var pitch = Math.floor( colorValue[0]/colorValue[1] );
-
-	pitch = Math.floor( map(pitch, 20,700, 0,8) );
-
-//	console.log(colorRGB.r+","+colorRGB.g+","+colorRGB.b + " – "+pitch);
-
-
-	if(lockMusic === false) {
-		conductor.setTimeSignature(1, 8);
-		conductor.setTempo(180);
-
-		var scale = ["C","D","E","F","G","A","B"];
-
-		var piano = conductor.createInstrument('square', 'oscillators');
-		var drum = conductor.createInstrument('white', 'noises');
-
-		drum.setVolume(25);
-
-		    // Bar 1
-
-//		piano.note('quarter', 'C5')
-//		    .rest('quarter');
-
-		for(var i = 0; i < (triangleAmount/8); i++) {
-			var note = scale[getRandom(2,6)]+pitch;
-
-			switch( getRandom(0,3) ) {
-
-				case 0:
-					piano.note('eighth', note);
-					drum.rest('eighth', note);
-					break;
-
-				case 1:
-					piano.rest('eighth', note);
-					drum.note('eighth', note);
-					break;
-
-				case 2:
-					piano.note('eighth', note);
-					drum.note('eighth', note);
-					break;
-
-				case 3:
-					piano.rest('eighth', note);
-					drum.rest('eighth', note);
-					break;
-			}
-		}
-
-
-//		piano.repeat(4);
-//		drum.repeat(4);
-
-
-		conductor.setOnFinishedCallback(function() {
-			lockMusic = false;
-		});
-
-		player = conductor.finish();
-
-		//player.loop(loop);
-
-		player.play();
-		lockMusic = true;
-		console.log(note);
-	}
-}
-*/
 
 $(document).ready(function() {
 
@@ -445,12 +327,7 @@ $(document).ready(function() {
 
 	$(document).on("mousestop", function() {
 
-		//$("body").css("cursor","pointer");
-		//if(!locked) {
-			generateLink(coordX, coordY);
-		//}
-
-		//partyTime = setInterval(changeMusic(), 10);
+		generateLink(coordX, coordY);
 	});
 
 	$(document).keypress(function(e) {
@@ -485,44 +362,7 @@ $(document).ready(function() {
 
 	});
 
-	$(document).keydown(function(e) {
-
-		// if turned "tab" on (16)
-		// if "shift" pressed (20)
-/*
-		if( e.which === 16 ) {
-
-			lockMusic = false;
-
-			//partyInterval = setInterval( function() {
-			changeMusic();
-			//},10);
-
-			$("div#saved").remove();
-			$("ul#controls li#lock").addClass("selected");
-		}
-*/
-	});
-
 	$(document).keyup(function(e) {
-		// if turned "tab" off (16)
-		// if "shift" released (20)
-/*
-		if( e.which === 16 ) {
-
-			lockMusic = true;
-
-			//clearInterval(partyInterval);
-			player.stop();
-
-
-
-			$("ul#controls li#lock").removeClass("selected");
-
-			lockedSelected.attr("fill",fill);
-			lockedSelected.attr("fill-opacity",opacity);
-		}
-*/
 
 		// if "Space bar" pressed
 		if( e.which === 32 ) {
@@ -551,21 +391,6 @@ $(document).ready(function() {
 		}
 	});
 
-/*
-	$("svg path").click(function() {
-
-		if(locked) {
-
-			$(this).fadeOut(500);
-
-			setTimeout(function() {
-				$(this).remove();
-			},500);
-
-		}
-	});
-*/
-
 	$("svg").children().hover(
 		function() {
 
@@ -587,31 +412,12 @@ $(document).ready(function() {
 		}
 	);
 
-	$("body").delegate("div#saved a", "click", function() {
+	$("body").delegate("div#saved span", "click", function() {
 
     var filename = Date.now() + ".png";
-		var imgData = $("div#saved a").attr("href").split(/,(.+)?/)[1];
-    var blob = b64toBlob(imgData, 'image/png');
+    var blob = generateLink();
 
+    window.saveAs(blob, "treep.png");
     uploadMedia(filename, blob);
 	});
-
-
-	/* MUSIC */
-/*
-	var conductor = new BandJS();
-	conductor.setTimeSignature(4,4);
-	conductor.setTempo(120);
-
-	var piano = conductor.createInstrument();
-
-	piano.note('quarter', 'C4');
-	piano.note('quarter', 'D4');
-	piano.note('quarter', 'E4');
-	piano.note('quarter', 'F4');
-
-	var player = conductor.finish();
-
-	player.play();
-*/
 });
